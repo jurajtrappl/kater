684994ce23fc46fe27e91967f19bea330ba05c65
Juraj Trappl <juraj.trappl@proton.me>
2024-06-21 18:09:14 +0200

    added explore action label

--- src/ui/label.py
+++ src/ui/label.py
@@ -2,6 +2,29 @@ import pygame
 
 from engine.player import Player
 
+class ExploreActionLabel:
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        text_color: pygame.color.Color
+    ) -> None:
+        self._x = x
+        self._y = y
+        self._text_color = text_color
+
+    def update(self, game_state) -> None:
+        if game_state["explore"] is None:
+            self._text_value = ""
+        else:
+            current_ticks = pygame.time.get_ticks()
+            remaining_ticks = game_state["explore"] - current_ticks
+            remaining_time = remaining_ticks // 1000
+            self._text_value = f"Remaining time: {remaining_time} seconds"
+
+    def draw(self, screen: pygame.Surface, font: pygame.font.Font):
+        image = font.render(self._text_value, True, self._text_color)
+        screen.blit(image, (self._x, self._y))
 
 class PlayerAttributeLabel:
     def __init__(
@@ -16,8 +39,8 @@ class PlayerAttributeLabel:
         self._property_name = property_name
         self._text_color = text_color
 
-    def update(self, player: Player) -> None:
-        self._property_value = getattr(player, self._property_name)
+    def update(self, game_state) -> None:
+        self._property_value = getattr(game_state["player"], self._property_name)
 
     def draw(self, screen: pygame.Surface, font: pygame.font.Font) -> None:
         label_text = f"{self._property_name.capitalize()}: {self._property_value}"
