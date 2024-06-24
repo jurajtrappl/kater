05ebbc147cc71f74939a410deb0f1caf7c2e6858
Juraj Trappl <juraj.trappl@proton.me>
2024-06-25 01:31:29 +0200

    rename because of not unique naming auth

--- backend/kater/kater/settings.py
+++ backend/kater/kater/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth.apps.AuthConfig',
+    'authentication.apps.AuthenticationConfig',
     'rest_framework'
 ]
 
