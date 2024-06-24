7e8c433c52d796b96865ba35b649c41533f852b8
Martin Mucha <martin98m@gmail.com>
2024-06-24 17:51:02 +0200

    docker be django

--- backend/kater/kater/settings.py
+++ backend/kater/kater/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'kater.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("PG_DB_HOST"),
+        'PORT': "5432",
     }
 }
 
