4d4990ffaac4c9e78abf62574dd7ef8d96154f42
Juraj Trappl <juraj.trappl@proton.me>
2024-07-06 07:13:04 +0200

    remove users app

--- backend/kater/kater/settings.py
+++ backend/kater/kater/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    'corsheaders',
+
     'rest_framework',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
