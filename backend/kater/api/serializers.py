a7fe24345c4553628995eddf5917971d27a101b2
Juraj Trappl <juraj.trappl@proton.me>
2024-07-06 15:48:29 +0200

    add hashing of a password

--- backend/kater/api/serializers.py
+++ backend/kater/api/serializers.py
@@ -6,14 +6,22 @@ from .models import Player
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = '__all__'
+        fields = "__all__"
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'password', 'username']
+        fields = ["id", "email", "password", "username"]
         extra_kwargs = {
             "password": {
                 "write_only": True
             }
         }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
